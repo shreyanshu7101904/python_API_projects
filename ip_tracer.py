1aa7a02fd86ca13cec1a65dc3ba822ec8fb33b70
Shreyanshu shankar <shreyanshu7101904@gmail.com>
2018-04-18 12:09:10 +0530

    Added user ip detection

--- ip_tracer.py
+++ ip_tracer.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import urllib.request
 
 
 print("*"*30)
@@ -10,8 +11,10 @@ print("*"*30)
 def tracker(ip_input):
     url = "http://ip-api.com/json/" + ip_input
     content = requests.get(url).json()
-    print("Query for IP",content["query"])
-    print(json.dumps(content,sort_keys=True,indent=4))
+    my_ip = urllib.request.urlopen('http://ip.42.pl/raw').read()
+    print("Starting Request from Your IP", my_ip.decode('utf-8'))
+    print("Query for IP", content["query"])
+    print(json.dumps(content, sort_keys=True, indent=4))
 
 
 x = str(input("Enter IP adress/Domain name :"))
